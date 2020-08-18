d6b2424892445a31cadd8d44777904d70dacc7de
Arob7 <66214652+Arob7@users.noreply.github.com>
2020-08-18 18:26:45 -0400

    added questions to employee.js

--- Develop/lib/Employee.js
+++ Develop/lib/Employee.js
@@ -1,1 +1,35 @@
 // TODO: Write code to define and export the Employee class
+// class Employee {
+//     constructor(name, strength, hitPoints)
+
+const questions = [
+  {
+    type: "input",
+    name: "name",
+    message: "What is your manager's name?",
+  },
+  {
+    type: "input",
+    name: "role",
+    message: "What is your role?",
+  },
+  {
+    type: "input",
+    name: "email",
+    message: "What is your manager's email",
+  },
+  {
+    type: "input",
+    name: "id",
+    message: "What is your manager's ID",
+  },
+  {
+    type: "list",
+    name: "employeeType",
+    message: "Which type of team member would you like to add?",
+    choices: [`Manager``Engineer`, `Intern`],
+  },
+
+  // function to write README file
+  //   function writeToFile(README, data) {}
+];
