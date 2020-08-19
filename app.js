5eb4c736e9c0e8185069da4b8f4a5dc79e455084
Arob7 <66214652+Arob7@users.noreply.github.com>
2020-08-18 21:48:55 -0400

    deleted Develop folder, Employee and Engineer files Pass test

--- app.js
+++ app.js
@@ -10,6 +10,38 @@ const outputPath = path.join(OUTPUT_DIR, "team.html");
 
 const render = require("./lib/htmlRenderer");
 
+const questions = [
+  {
+    type: "input",
+    name: "name",
+    message: "What is your manager's name?",
+  },
+  {
+    type: "input",
+    name: "role",
+    message: "What is your role?",
+  },
+  {
+    type: "input",
+    name: "email",
+    message: "What is your manager's email",
+  },
+  {
+    type: "input",
+    name: "id",
+    message: "What is your manager's ID",
+  },
+  {
+    type: "list",
+    name: "employeeType",
+    message: "Which type of team member would you like to add?",
+    choices: [`Manager``Engineer`, `Intern`],
+  },
+
+  // function to write README file
+  //   function writeToFile(README, data) {}
+];
+
 // Write code to use inquirer to gather information about the development team members,
 // and to create objects for each team member (using the correct classes as blueprints!)
 
